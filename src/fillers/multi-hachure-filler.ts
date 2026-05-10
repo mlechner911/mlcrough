d1634953ddc119f4df306d4dd0965a78bcfe3d92
Michael Lechner <xsltwonder@gmail.com>
2026-05-10 12:43:09 +0200

    feat: enhance MultiHachureFiller with randomized line distribution
    - Use Fisher-Yates shuffle on hachure lines for a more natural look
    - Expand to 4 opacity levels [0.4, 0.5, 0.6, 0.9]
    - Respect internal randomizer for deterministic seeding

--- src/fillers/multi-hachure-filler.ts
+++ src/fillers/multi-hachure-filler.ts
@@ -15,13 +15,21 @@ export class MultiHachureFiller implements PatternFiller {
 
   fillPolygons(polygonList: Point[][], o: ResolvedOptions): OpSet[] {
     const lines = polygonHachureLines(polygonList, o);
-    
-    // Group lines into 3 sets with different opacities
-    const groups: Op[][] = [[], [], []];
-    const opacities = [0.2, 0.5, 0.8];
 
-    lines.forEach((line, i) => {
-      const groupIdx = i % 3;
+    // Shuffle lines to distribute opacities randomly across the shape
+    const shuffledLines = [...lines];
+    const random = () => o.randomizer ? o.randomizer.next() : Math.random();
+    
+    for (let i = shuffledLines.length - 1; i > 0; i--) {
+      const j = Math.floor(random() * (i + 1));
+      [shuffledLines[i], shuffledLines[j]] = [shuffledLines[j], shuffledLines[i]];
+    }
+
+    const opacities = [0.4, 0.5, 0.6, 0.9];
+    const groups: Op[][] = opacities.map(() => []);
+
+    shuffledLines.forEach((line, i) => {
+      const groupIdx = i % opacities.length;
       groups[groupIdx].push(...this.helper.doubleLineOps(line[0][0], line[0][1], line[1][0], line[1][1], o));
     });
 
